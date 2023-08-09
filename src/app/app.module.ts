1d43b55ff540bff150d0a431d4a0bbf1859849e2
martinsilva10 <135297173+martinsilva10@users.noreply.github.com>
2023-08-08 23:59:55 -0300

    tp final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ClientsComponent } from './clients/clients.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsSectionComponent } from './clients-section/clients-section.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterrComponent } from './footerr/footerr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    ClientsComponent,
+    TeamComponent,
+    ClientsSectionComponent,
+    ContactComponent,
+    FooterrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footerr/footerr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footerr',
+  templateUrl: './footerr.component.html',
+  styleUrls: ['./footerr.component.scss']
+})
+export class FooterrComponent {
+
+}
